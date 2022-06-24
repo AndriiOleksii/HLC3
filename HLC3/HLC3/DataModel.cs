6a5008685142b6eabc7699dd8558252b88067d39
AndriiOleksii <106560705+AndriiOleksii@users.noreply.github.com>
2022-06-24 16:20:08 +0300

    Add files via upload

---
+++ HLC3/HLC3/DataModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HLC3
+{
+    public class DataModel
+    {
+        public string client_id { get; set; }
+        public List<DataModelEvent> events { get; set; }
+    }
+
+    public class DataModelEvent
+    {
+        public string name { get; set; }
+        
+        [JsonProperty("params")]
+        public ParamsObject parameters { get; set; }
+    }
+
+    public class ParamsObject
+    {
+        public string ratio { get; set; }
+    }
+}
---
+++ HLC3/HLC3/Program.cs
@@ -0,0 +1,38 @@
+﻿using HLC3;
+using Newtonsoft.Json;
+using RestSharp;
+
+string API_SECRET = "jqrVveuIQNWI5p1h6dA7Vw";
+string M_ID = "G-ZQPGRQ2YB5";
+string CLIENT_ID = "1277594820.1633351942";
+string UA_HEADER = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36";
+
+// Test logic - тут буде відбуватися виклик до апі/бази, яка має цю інфу
+var ratio = 35;
+
+var jsonData = JsonConvert.SerializeObject(new DataModel()
+{
+    client_id = CLIENT_ID,
+    events = new List<DataModelEvent>()
+    {
+        new DataModelEvent()
+        {
+            name = "test",
+            parameters = new ParamsObject()
+            {
+                ratio = ratio.ToString()
+            }
+        }
+    }
+});
+
+var restClient = new RestSharp.RestClient();
+
+var restCall = new RestSharp.RestRequest($"https://www.google-analytics.com/mp/collect?measurement_id={M_ID}&api_secret={API_SECRET}", 
+    Method.Post);
+
+restCall.AddHeader("User-Agent", UA_HEADER);
+
+restCall.AddJsonBody(jsonData);
+
+await restClient.ExecuteAsync(restCall);
